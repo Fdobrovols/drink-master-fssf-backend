ecb3803dd4408f43c2778d801bd192a701362455
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-26 18:26:59 +0300

    fix getByCategory and getById controllers and routes

--- controllers/recipes-controllers/getById.js
+++ controllers/recipes-controllers/getById.js
@@ -3,12 +3,12 @@ import { Recipe } from "../../models/recipe/index.js";
 const successStatus = 200;
 
 const getById = async (req, res) => {
-  const { id } = req.params;
   console.log(req.params);
+  const { id } = req.params;
 
   const result = await Recipe.findById(id);
 
-  //   res.status(successStatus).json(`it\`s id: ${id}`);
+  res.status(successStatus).json(result);
 };
 
 export default getById;
