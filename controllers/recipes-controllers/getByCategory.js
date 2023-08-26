226aafcfe1942647170330c3515e88d73c60cd08
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-27 00:46:41 +0300

    add searchRouter, ingredientRouter, fix getById and getByCategory functions

--- controllers/recipes-controllers/getByCategory.js
+++ controllers/recipes-controllers/getByCategory.js
@@ -4,7 +4,7 @@ const successStatus = 200;
 
 const getByCategory = async (req, res) => {
   console.log(req.params);
-  const { categoryName: category } = req.params;
+  const { category } = req.params;
 
   const { page = 1, limit = 8, ...query } = req.query;
   const skip = (page - 1) * limit;
