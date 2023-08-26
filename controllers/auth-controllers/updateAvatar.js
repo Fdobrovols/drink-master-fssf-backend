cb4cdda9cc7511be1ab000975e584ea221d88749
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-26 17:49:21 +0300

    add cloudinary config and avatars config during uploading.

--- controllers/auth-controllers/updateAvatar.js
+++ controllers/auth-controllers/updateAvatar.js
@@ -1,54 +1,37 @@
-import { v2 as cloudinary } from "cloudinary";
 import fs from "fs/promises";
-import "dotenv/config";
 
 import { User } from "../../models/user/index.js";
-import { HttpError } from "../../helpers/index.js";
-
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_SECRET, CLOURINARY_API_KEY } = process.env;
-
-cloudinary.config({
-  cloud_name: CLOUDINARY_CLOUD_NAME,
-  api_key: CLOURINARY_API_KEY,
-  api_secret: CLOUDINARY_SECRET,
-});
+import { HttpError, cloudinary } from "../../helpers/index.js";
 
 const updateAvatar = async (req, res) => {
-  // console.log(cloudinary);
-
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
 
-  // await avatarHandler(tempUpload);
-
-  const fileName = `${_id}_${originalname}`;
-
-  // const resultUpload = path.join(tempUpload, fileName);
-
-  // await fs.rename(tempUpload, resultUpload);
-
-  const { url } = await cloudinary.uploader.upload(
-    tempUpload,
-    {
+  try {
+    const fileName = `${_id}_${originalname}`;
+    const { url: avatarURL } = await cloudinary.uploader.upload(tempUpload, {
+      folder: "avatars",
       public_id: fileName,
-      width: 250,
-      height: 250,
-    },
-    function (error, result) {
-      // console.log(result);
-      return result;
+      aspect_ratio: "1.0",
+      gravity: "face",
+      height: 200,
+      zoom: "0.75",
+      crop: "thumb",
+      radius: "max",
+    });
+    await fs.unlink(tempUpload);
+
+    const user = await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
+
+    if (!user) {
+      throw HttpError(401);
     }
-  );
 
-  await fs.unlink(tempUpload);
-
-  const user = await User.findByIdAndUpdate(_id, { avatarURL: url }, { new: true });
-
-  if (!user) {
-    throw HttpError(401);
+    res.json({ avatarURL: user.avatarURL });
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
   }
-
-  res.json({ avatarURL: user.avatarURL });
 };
 
 export default updateAvatar;
