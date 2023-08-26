492fc42b2f25cd06595a7d30112b43a965111c5f
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-26 17:50:09 +0300

    remove avatarHandler func, update token lifetime and add password patterns to joi validation

--- schemas/users-schemas/login-schema.js
+++ schemas/users-schemas/login-schema.js
@@ -1,14 +1,17 @@
 import Joi from "joi";
-import { emailRegexp } from "../../constants/index.js";
+import { emailRegexp, passwordRegexp } from "../../constants/index.js";
 
 const userLoginSchema = Joi.object({
   email: Joi.string()
     .pattern(emailRegexp)
     .required()
     .messages({ "any.required": "missing required field email" }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": `password should have a minimum length of {#limit}`,
+  password: Joi.string().min(6).max(16).pattern(passwordRegexp).required().messages({
     "any.required": "missing required field password",
+    "string.min": `password should have a minimum length of {#limit}`,
+    "string.max": "{{#label}} length must be less than or equal to {{#limit}} characters long",
+    "string.pattern.base":
+      "{:[.]} does not meet the requirements. Password must contain at least 1 capital letter, 1 small letter and 1 number",
   }),
 });
 
--- schemas/users-schemas/signup-schema.js
+++ schemas/users-schemas/signup-schema.js
@@ -1,5 +1,5 @@
 import Joi from "joi";
-import { emailRegexp } from "../../constants/index.js";
+import { emailRegexp, passwordRegexp } from "../../constants/index.js";
 
 const userSignupSchema = Joi.object({
   name: Joi.string().required().messages({ "any.required": "missing required field name" }),
@@ -7,9 +7,12 @@ const userSignupSchema = Joi.object({
     .pattern(emailRegexp)
     .required()
     .messages({ "any.required": "missing required field email" }),
-  password: Joi.string().min(6).required().messages({
-    "string.min": `password should have a minimum length of {#limit}`,
+  password: Joi.string().min(6).max(16).pattern(passwordRegexp).required().messages({
     "any.required": "missing required field password",
+    "string.min": `password should have a minimum length of {#limit}`,
+    "string.max": "{{#label}} length must be less than or equal to {{#limit}} characters long",
+    "string.pattern.base":
+      "{:[.]} does not meet the requirements. It must contain at least 1 capital letter, 1 small letter and 1 number",
   }),
 });
 
