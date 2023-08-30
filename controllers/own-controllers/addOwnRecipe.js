9315a7c52b558067006f3b7789f04c138adaf503
Mikhailo Salko <slipknotmisha@gmail.com>
2023-08-30 19:05:37 +0300

    updare own routes, add to addOwnRecipe controller cloudinary upload for files.

--- controllers/own-controllers/addOwnRecipe.js
+++ controllers/own-controllers/addOwnRecipe.js
@@ -1,9 +1,29 @@
+import fs from "fs/promises";
 import { Recipe } from "../../models/recipe/index.js";
+import { HttpError, cloudinary } from "../../helpers/index.js";
 
 const addOwnRecipe = async (req, res) => {
   const { _id: owner } = req.user;
-  const newRecipe = await Recipe.create({ ...req.body, owner });
-  res.status(201).json(newRecipe);
+  const { path: tempUpload, originalname } = req.file;
+
+  try {
+    const fileName = `${owner}_${originalname}`;
+    const { url: drinkThumb } = await cloudinary.uploader.upload(tempUpload, {
+      folder: "recipes",
+      public_id: fileName,
+      quality: 60,
+      crop: "fill",
+    });
+
+    const newRecipe = await Recipe.create({ ...req.body, drinkThumb, owner });
+
+    await fs.unlink(tempUpload);
+
+    res.status(201).json(newRecipe);
+  } catch (error) {
+    await fs.unlink(tempUpload);
+    throw error;
+  }
 };
 
 export default addOwnRecipe;
