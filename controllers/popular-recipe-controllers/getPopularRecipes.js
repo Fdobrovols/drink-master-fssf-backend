df27475dc72f70c591516ced99f0532c8d9b32f5
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-08-31 01:09:59 +0200

    Fixed getAllRecipes & getPopularRecipes controllers

--- controllers/popular-recipe-controllers/getPopularRecipes.js
+++ controllers/popular-recipe-controllers/getPopularRecipes.js
@@ -1,9 +1,17 @@
 import { Recipe } from "../../models/recipe/index.js";
 
+const defaultDocNumber = 4;
+
 const getPopularRecipes = async (req, res, next) => {
+  const isNumberSet = Boolean(req.query.number_of_popular);
+
+  const docNumber = isNumberSet
+    ? Number(req.query.number_of_popular)
+    : defaultDocNumber;
+
   const result = await Recipe.aggregate([
     { $sort: { favorite: -1 } },
-    { $limit: 10 },
+    { $limit: docNumber },
   ]);
 
   res.json(result);
