c90a6425318e33de8054b9e66b714ed0db769966
Mikhailo Salko <slipknotmisha@gmail.com>
2023-09-02 22:43:27 +0300

    add avatarURL in res in updateUser controller, add parsing for req.body.ingredinets

--- controllers/own-controllers/addOwnRecipe.js
+++ controllers/own-controllers/addOwnRecipe.js
@@ -1,9 +1,18 @@
 import fs from "fs/promises";
 import { Recipe } from "../../models/recipe/index.js";
-import { HttpError, cloudinary } from "../../helpers/index.js";
+import { cloudinary } from "../../helpers/index.js";
 
 const addOwnRecipe = async (req, res) => {
+  console.log(req.body);
+  console.log(req.file);
+
   const { _id: owner } = req.user;
+  if (!req.file) {
+    const newRecipe = await Recipe.create({ ...req.body, owner });
+    console.log(newRecipe);
+    return req.json(newRecipe);
+  }
+
   const { path: tempUpload, originalname } = req.file;
 
   try {
@@ -15,7 +24,14 @@ const addOwnRecipe = async (req, res) => {
       crop: "fill",
     });
 
-    const newRecipe = await Recipe.create({ ...req.body, drinkThumb, owner });
+    const parsedIngredients = JSON.parse(req.body.ingredients);
+
+    const newRecipe = await Recipe.create({
+      ...req.body,
+      ingredients: [...parsedIngredients],
+      drinkThumb,
+      owner,
+    });
 
     await fs.unlink(tempUpload);
 
