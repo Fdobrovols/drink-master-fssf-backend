2b447f6acf3c6791c465045eb057daa6185311f2
Oleksandr Moskaliuk <olekspm@gmail.com>
2023-09-02 19:43:18 +0200

    Fixed controllers

--- controllers/favorite-controllers/getFavorite.js
+++ controllers/favorite-controllers/getFavorite.js
@@ -1,22 +1,19 @@
 import { Recipe } from "../../models/recipe/index.js";
 
 const getFavorite = async (req, res) => {
-  const { page = 1, limit = 2 } = req.query;
+  const { page = 1, limit = 9 } = req.query;
   const { _id } = req.user;
 
   const skip = (page - 1) * limit;
 
-  // const favorite = await Recipe.aggregate([
-  //   { $match: { favorite: _id } },
-  //   { $count: "totalHits" },
-  //   { $skip: Number(skip) },
-  //   { $limit: Number(limit) },
-  // ]);
+  const result = await Recipe.find({ favorite: _id }, "", {
+    skip,
+    limit,
+  }).lean();
 
-  const result = await Recipe.find({ favorite: _id }, "", { skip, limit }).lean();
   const totalHits = await Recipe.countDocuments({ favorite: _id });
 
-  res.json({ data: result, page, limit, totalHits });
+  res.json({ page, limit, totalHits, result });
 };
 
 export default getFavorite;
