dc87cc643678945a8b37983b3c06ad4a0c021385
Mikhailo Salko <slipknotmisha@gmail.com>
2023-09-06 00:04:40 +0300

    update controller addRecipe and add parsing ingredients in validationBody recorator

--- decorators/validateBody.js
+++ decorators/validateBody.js
@@ -2,6 +2,11 @@ import { HttpError } from "../helpers/index.js";
 
 const validateBody = (schema, message) => {
   const func = (req, _, next) => {
+    if (req.body.ingredients && typeof req.body.ingredients === "string") {
+      const parsedIngredients = JSON.parse(req.body.ingredients);
+      req.body.ingredients = [...parsedIngredients];
+    }
+
     const { error } = schema.validate(req.body);
 
     if (error) {
